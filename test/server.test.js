a07bf45745eae7038019fe96336133858e9a5077
Mark Smyth <xtruckdriver1@gmail.com>
2025-03-11 13:21:58 -0400

    GET-brands-id-products: add tests

--- test/server.test.js
+++ test/server.test.js
@@ -20,6 +20,23 @@ describe('/GET brands', () => {
   });
 });
 
+describe('/GET brands/:id/products', () => {
+  it('should return array of products for a given brand', (done) => {
+    const targetId = brands[0].id;
+
+    chai
+      .request(server)
+      .get(`brands/${targetId}/products`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.every(product => product.id === targetId).should.be.true;
+        done();
+      });
+    
+  });
+});
+
 describe('Login', () => {});
 
 describe('Cart', () => {});
